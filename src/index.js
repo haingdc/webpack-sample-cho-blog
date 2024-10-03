64944de3b326e05b0f8a8c6385e77237482260d5
Hai.Nguyen <haingdc@gmail.com>
2024-10-03 22:39:27 +0700

    Add Yarn configuration, update package dependencies, remove obsolete script imports, and enhance Webpack setup for improved builds

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import(/* webpackChunkName: "async-a" */ "./a");
-import(/* webpackChunkName: "async-b" */ "./b");
-import(/* webpackChunkName: "async-c" */ "./c");
+import { oneTrueDate } from "@hn/one-true-date";
+import { oneTrueDate as oneTrueDateXiKhoi } from "@hn/one-true-date-verpa-xi-khoi";
+
+console.log(oneTrueDate(new Date()));
+console.log(oneTrueDateXiKhoi(new Date()));
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,55 @@
 const path = require("path");
+const StatoscopeWebpackPlugin = require("@statoscope/webpack-plugin").default;
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const glob = require("glob");
 
 /**
  * @type {import("webpack/types").Configuration}
  */
 module.exports = {
-  entry: "./src/index.js",
+  mode: "production",
+  entry: {
+    main: "./src/index.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: "[name].initial.js",
     clean: true,
+    chunkFilename: "[name].not-initial.js",
   },
-  resolve: {
-    modules: [path.resolve(__dirname, "src/node_modules")],
-  },
+  // resolve: {
+  //   modules: [path.resolve(__dirname, "src/node_modules")],
+  // },
   optimization: {
     minimize: false,
     moduleIds: "named",
     chunkIds: "named",
     splitChunks: false,
   },
+  stats: {
+    all: true, // enable all the stats
+  },
+  plugins: [
+    // new CleanWebpackPlugin({
+    //   dry: false,
+    //   verbose: false,
+    //   cleanOnceBeforeBuildPatterns: ["../analyze/*"],
+    //   cleanOnceBeforeBuildPatterns: [],
+    //   dangerouslyAllowCleanPatternsOutsideProject: true,
+    // }),
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+    }),
+    new StatoscopeWebpackPlugin({
+      name: "stat-webpack-sample",
+      open: false,
+      compressor: "gzip",
+      saveReportTo: "analyze/report.html",
+      saveStatsTo: "analyze/stats/[name]-[hash].json",
+      normalizeStats: false,
+      reports: [],
+      additionalStats: glob.sync("./analyze/stats/*.json"),
+    }),
+  ],
 };
